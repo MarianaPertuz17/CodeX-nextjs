4c4edd215cd57f6ea53d25daa12a1af8ab6cbf63
Mariana <marianapertuzv@gmail.com>
2022-08-14 17:29:21 -0500

    style: icons for stopwatch

--- components/timer/index.jsx
+++ components/timer/index.jsx
@@ -4,6 +4,7 @@ import pauseIcon from '../../public/assets/images/pause.png';
 import resetIcon from '../../public/assets/images/reset.png';
 import Image from 'next/image';
 import styles from './styles.module.css'
+import { useState } from 'react';
 
 export function MyStopwatch() {
   const {
@@ -13,7 +14,23 @@ export function MyStopwatch() {
     start,
     pause,
     reset,
-  } = useStopwatch({ autoStart: true });
+  } = useStopwatch({ autoStart: false });
+
+  const [ started, setStarted ] = useState(false);
+
+  const handlePlay = () => {
+    if (!started) {
+      start();
+    } else {
+      pause();
+    }
+    setStarted(prevState => !prevState);
+  }
+
+  const handleReset = () => {
+    reset(undefined, false); 
+    setStarted(false);
+  }
 
 
   return (
@@ -21,23 +38,25 @@ export function MyStopwatch() {
       <div style={{fontSize: '18px', color:'white', fontFamily:'Open Sans', marginRight:'10%'}}>
         <span>{hours < 10 ? `0${hours}`: hours}</span>:<span>{minutes< 10 ? `0${minutes}`: minutes}</span>:<span>{seconds < 10 ? `0${seconds}`: seconds}</span>
       </div>
-      <button onClick={start} className={styles.button}>
-        <Image
-          src={playIcon}
-          alt="play"
-          layout='fill'
-          objectFit='contain'
-        />
-      </button>
-      <button onClick={pause} className={styles.button}>
-      <Image
+      <button onClick={handlePlay} className={styles.button}>
+        {started ? (
+          <Image
           src={pauseIcon}
           alt="pause"
           layout='fill'
           objectFit='contain'
-        />
+          />) : 
+          (
+          <Image
+          src={playIcon}
+          alt="play"
+          layout='fill'
+          objectFit='contain'
+          />
+          )}
+        
       </button>
-      <button onClick={reset} className={styles.button}>
+      <button onClick={handleReset} className={styles.button}>
       <Image
           src={resetIcon}
           alt="reset"
