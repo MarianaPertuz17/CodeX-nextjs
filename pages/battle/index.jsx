569c397594b92309227389b5c8399fb1e7c4b873
Mariana <marianapertuzv@gmail.com>
2022-08-16 09:36:35 -0500

    fix: fixed problem with height in leaves code

--- pages/battle/index.jsx
+++ pages/battle/index.jsx
@@ -20,7 +20,7 @@ export default function CSSBattle () {
     div {
       width: 100px;
       height: 100px;
-      background: #dd6b4d;
+      background: blueviolet;
     }
   </style>
   
@@ -63,7 +63,7 @@ export default function CSSBattle () {
                 <div className={styles.label}>Output</div>
               </div>
               <div className={styles.outputContainer}>
-                <iframe srcDoc={codeString} height={300} width={400}></iframe>
+              <iframe srcDoc={codeString} style={{background:'white', width:'400px',height:'300px',border:'0',outline:'0', overflow:'hid' }}></iframe>
               </div>
             </div>
 
@@ -72,7 +72,7 @@ export default function CSSBattle () {
                 <div className={styles.label}>Target</div>
               </div>
               <div className={styles.outputContainer}>
-                <iframe src="https://cssbattle.dev/targets/25.png" height={300} width={400}></iframe>
+                <iframe src="https://cssbattle.dev/targets/25.png" style={{background:'white', width:'400px',height:'300px',border:'0',outline:'0', }}></iframe>
               </div>
             </div>
             
