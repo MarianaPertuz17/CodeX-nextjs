ba3d1252354eb8a9dac307a652aebc47c5d64502
michael <michael.shaevitch@gmail.com>
2022-08-17 16:19:14 -0400

    Re-added the deleted code in exercises.js

--- pages/api/exercises.js
+++ pages/api/exercises.js
@@ -1,1 +1,12 @@
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient()
 
+
+export default async function handler(req, res) {
+  try{
+      const exercise = await prisma.exercise.findMany()
+    res.json(exercise)
+  }catch(error){
+      res.status(400).json({error})
+  }
+}
