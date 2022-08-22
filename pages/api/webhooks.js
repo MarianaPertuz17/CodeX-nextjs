4ee3aadf605208c3da729cdcb6cdfa80138a245a
michael <michael.shaevitch@gmail.com>
2022-08-21 21:28:19 -0400

    Implemented Stripe checkout functionality (with Webhooks)

--- pages/api/webhooks.js
+++ pages/api/webhooks.js
@@ -1,5 +1,7 @@
 import Stripe from 'stripe';
 import { buffer } from 'micro';
+import { prisma } from '../../db.js';
+// import { useUser } from '@auth0/nextjs-auth0';
 
 export const config = {
   api: {
@@ -9,6 +11,7 @@ export const config = {
 
 export default async function webhookHandler (req, res) {
   const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+  // const { user } = useUser();
 
   if (req.method === 'POST') {
     const buf = await buffer(req);
@@ -26,8 +29,30 @@ export default async function webhookHandler (req, res) {
       return res.status(400).send(`Webhook error: ${error.message}`);
     }
 
-    // console.log('event', event);
-    console.log('WEBHOOK');
+    if (event.type === 'checkout.session.completed') {
+      console.log('pi', event.data.object.payment_intent);
+      await prisma.user.update({
+        where: {
+          authId: event.data.object.metadata.authUserId,
+        },
+        data: {
+          stripeId: event.data.object.payment_intent,
+        },
+      });
+    }
+
+    if (event.type === 'payment_intent.succeeded') {
+      console.log(event.data.object.id, `PAYMENT SUCCESS!!!`);
+      await prisma.user.update({
+        where: {
+          stripeId: event.data.object.id,
+        },
+        data: {
+          paidUser: true,
+        },
+      });
+      
+    }
 
     res.status(200).send();
   }
