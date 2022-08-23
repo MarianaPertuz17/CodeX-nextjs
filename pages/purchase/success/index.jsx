bc0df7cebf56542392fe7311b40e62415cbbc30c
michael <michael.shaevitch@gmail.com>
2022-08-23 19:34:39 -0400

    Added width and height to confetti

--- pages/purchase/success/index.jsx
+++ pages/purchase/success/index.jsx
@@ -1,11 +1,16 @@
 import styles from './styles.module.css';
 import { NavBar } from '../../../components/navBar';
-import Confetti from 'react-confetti'
+import useWindowSize from 'react-use/lib/useWindowSize';
+import Confetti from 'react-confetti';
 
 export default function purchaseSuccess () {
+  const { width, height } = useWindowSize();
   return (
     <div className={styles.container}>
-      <Confetti/>
+      <Confetti
+        width={width}
+        height={height}
+      />
       <NavBar/>
       <div className={styles.successFlex}>
         <div className={styles.successText}>Thank you, your order has been placed!</div>
