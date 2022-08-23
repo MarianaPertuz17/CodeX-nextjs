5e745c100a916ceb52b86e0700921bd5a6fff7ec
michael <michael.shaevitch@gmail.com>
2022-08-23 19:27:04 -0400

    Added confetti to the payment success page

--- pages/purchase/success/index.jsx
+++ pages/purchase/success/index.jsx
@@ -1,9 +1,11 @@
 import styles from './styles.module.css';
 import { NavBar } from '../../../components/navBar';
+import Confetti from 'react-confetti'
 
 export default function purchaseSuccess () {
   return (
     <div className={styles.container}>
+      <Confetti/>
       <NavBar/>
       <div className={styles.successFlex}>
         <div className={styles.successText}>Thank you, your order has been placed!</div>
