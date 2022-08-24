af7a82b6938ee1dfa801d9a92ea4bc4392b03a26
Mariana <marianapertuzv@gmail.com>
2022-08-23 22:27:01 -0500

    changed from localhost

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-export const url= 'http://localhost:3000/api';
+export const url= 'https://code-x-nextjs.vercel.app/api';
--- next.config.js
+++ next.config.js
@@ -3,8 +3,8 @@ const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
 }
-const withCSS = require('@zeit/next-css');
-module.exports = withCSS({
+// const withCSS = require('@zeit/next-css');
+// module.exports = withCSS({
 
-});
+// });
 module.exports = nextConfig
