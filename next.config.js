79fe86c5b5a8204074b175b8fb1f69ecafb5a7a0
Mariana <marianapertuzv@gmail.com>
2022-08-23 22:23:50 -0500

    fixing issues

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
-
+const nextConfig = {
+  reactStrictMode: false,
+  swcMinify: true,
+}
 const withCSS = require('@zeit/next-css');
 module.exports = withCSS({
 
 });
+module.exports = nextConfig
