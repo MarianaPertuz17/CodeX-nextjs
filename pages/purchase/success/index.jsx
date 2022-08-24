0640e3824dc613a10172a515c30a7152ba3de5ec
michael <michael.shaevitch@gmail.com>
2022-08-23 20:32:13 -0400

    The confetti worksclear

--- pages/purchase/success/index.jsx
+++ pages/purchase/success/index.jsx
@@ -1,15 +1,23 @@
 import styles from './styles.module.css';
 import { NavBar } from '../../../components/navBar';
-import useWindowSize from 'react-use/lib/useWindowSize';
 import Confetti from 'react-confetti';
+import { useEffect, useState } from 'react';
 
 export default function Success () {
-  const { width, height } = useWindowSize();
+
+  const [windowWidth, setWindowWidth] = useState(0);
+  const [windowHeight, setWindowHeight] = useState(0);
+
+  useEffect(() => {
+    setWindowWidth(window.width);
+    setWindowHeight(window.height);
+  }, [])
+
   return (
     <div className={styles.container}>
       <Confetti
-        width={width}
-        height={height}
+        width={windowWidth}
+        height={windowHeight}
       />
       <NavBar/>
       <div className={styles.successFlex}>
